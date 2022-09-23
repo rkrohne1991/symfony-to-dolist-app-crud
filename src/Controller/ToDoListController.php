cd05856b9ceddfa0186d77a43d9e0249aa70e06d
Rafal <rafal.krohne@hotmail.co.uk>
2022-09-23 15:27:24 +0200

    Adding packages, creating Task entity, adding data to database

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,9 +21,20 @@ class ToDoListController extends AbstractController
     /**
      * @Route("/create", name="create_task", methods={"POST"})
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        exit('to do: create a new task!');
+        $title = trim($request->request->get('title'));
+        if(empty($title)) { return $this->redirectToRoute('app_to_do_list'); }
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task;
+        $task->setTitle($title);
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_to_do_list');
     }
 
     /**
